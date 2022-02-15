9fd6954daa0ece10b1e22367f91d5dff6e170023
yoshiryo <ryota.yoshizaki.2r@stu.hosei.ac.jp>
2022-02-15 22:31:35 +0900

    追加するファイルの範囲を指定できるように変更

--- make_dir.py
+++ make_dir.py
@@ -4,12 +4,16 @@ import pathlib
 name = input("追加したいディレクトリの名前を入力 : ")
 dir_name = "C:\\Users\\ryota\\Desktop\\atcoder\\" + name + "\\" + name
 first_num, end_num = map(int, input("追加するディレクトリの数字を入力 : ").split())
-
+start_file_name, end_file_name = input("追加するファイル(a~oまで)の名前を入力 : ").split()
+alp = "abcdefghijklmno"
+satrt_index = alp.find(start_file_name)
+end_index = alp.find(end_file_name)
 for num in range(first_num, end_num + 1):
     num = str(num).zfill(3)
     new_dir = dir_name + num + "\\"
     os.mkdir(new_dir)
-    for file in ["a.py", "b.py", "c.py"]:
+    for i in range(satrt_index, end_index+1):
+        file = alp[i] + ".py"
         new_file = new_dir + file
         p = pathlib.Path(new_file)
         p.touch()
