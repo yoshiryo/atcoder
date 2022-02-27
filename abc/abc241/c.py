998f043ea8c08abfa1e686ac594cbc6acf50dcf4
yoshiryo <ryota.yoshizaki.2r@stu.hosei.ac.jp>
2022-02-27 22:37:53 +0900

    abc241のcを解いた

--- abc/abc241/c.py
+++ abc/abc241/c.py
@@ -1,2 +1,22 @@
 n = int(input())
 s = [input() for _ in range(n)]
+dxs = [0, 1, 1, 1]
+dys = [1, 1, 0, -1]
+for i in range(n):
+    for j in range(n):
+        for d in range(4):
+            dx = dxs[d]
+            dy = dys[d]
+            cnt = 0
+            for k in range(6):
+                nx = i + dx * k
+                ny = j + dy * k
+                if not (0 <= nx < n and 0 <= ny < n):
+                    cnt = 100
+                    break
+                if s[nx][ny] == ".":
+                    cnt += 1
+            if cnt <= 2:
+                print("Yes")
+                exit()
+print("No")
